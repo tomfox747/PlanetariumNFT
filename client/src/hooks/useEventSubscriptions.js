fe44b1701e9b70447f956a7157394031469e6fd0
Thomas Fox <47598118+tomfox747@users.noreply.github.com>
2022-01-17 23:29:11 +0000

    added all token mint events

--- client/src/hooks/useEventSubscriptions.js
+++ client/src/hooks/useEventSubscriptions.js
@@ -4,7 +4,24 @@ import { toast } from 'react-toastify';
 const useEventSubscriptions = (account) => {
 
     //Mint events
-    useMoralisSubscription("NFTmilkyway", q => q.equalTo("sender", account), [], {onCreate: () => toast("Your NFT was successfully minted", {type:'success'})})
+    useMoralisSubscription("MintedNFTmilkyway", q => q.equalTo("sender", account), [], {onCreate: () => toast("Your NFT was successfully minted", {type:'success'})})
+    useMoralisSubscription("MintedNFTmessier", q => q.equalTo("sender", account), [], {onCreate: () => toast("Your NFT was successfully minted", {type:'success'})})
+    useMoralisSubscription("MintedNFTandromeda", q => q.equalTo("sender", account), [], {onCreate: () => toast("Your NFT was successfully minted", {type:'success'})})
+    useMoralisSubscription("MintedNFTbetelguese", q => q.equalTo("sender", account), [], {onCreate: () => toast("Your NFT was successfully minted", {type:'success'})})
+    useMoralisSubscription("MintedNFTarcturus", q => q.equalTo("sender", account), [], {onCreate: () => toast("Your NFT was successfully minted", {type:'success'})})
+    useMoralisSubscription("MintedNFTformahaut", q => q.equalTo("sender", account), [], {onCreate: () => toast("Your NFT was successfully minted", {type:'success'})})
+    useMoralisSubscription("MintedNFTearth", q => q.equalTo("sender", account), [], {onCreate: () => toast("Your NFT was successfully minted", {type:'success'})})
+    useMoralisSubscription("MintedNFTmars", q => q.equalTo("sender", account), [], {onCreate: () => toast("Your NFT was successfully minted", {type:'success'})})
+    useMoralisSubscription("MintedNFTsaturn", q => q.equalTo("sender", account), [], {onCreate: () => toast("Your NFT was successfully minted", {type:'success'})})
+    useMoralisSubscription("MintedNFTthemoon", q => q.equalTo("sender", account), [], {onCreate: () => toast("Your NFT was successfully minted", {type:'success'})})
+    useMoralisSubscription("MintedNFTtitan", q => q.equalTo("sender", account), [], {onCreate: () => toast("Your NFT was successfully minted", {type:'success'})})
+    useMoralisSubscription("MintedNFTphobos", q => q.equalTo("sender", account), [], {onCreate: () => toast("Your NFT was successfully minted", {type:'success'})})
+    useMoralisSubscription("MintedNFTorion", q => q.equalTo("sender", account), [], {onCreate: () => toast("Your NFT was successfully minted", {type:'success'})})
+    useMoralisSubscription("MintedNFTcapricornus", q => q.equalTo("sender", account), [], {onCreate: () => toast("Your NFT was successfully minted", {type:'success'})})
+    useMoralisSubscription("MintedNFTscorpius", q => q.equalTo("sender", account), [], {onCreate: () => toast("Your NFT was successfully minted", {type:'success'})})
+    useMoralisSubscription("MintedNFToumuamua", q => q.equalTo("sender", account), [], {onCreate: () => toast("Your NFT was successfully minted", {type:'success'})})
+    useMoralisSubscription("MintedNFThalleyscomet", q => q.equalTo("sender", account), [], {onCreate: () => toast("Your NFT was successfully minted", {type:'success'})})
+    useMoralisSubscription("MintedNFTholmberg", q => q.equalTo("sender", account), [], {onCreate: () => toast("Your NFT was successfully minted", {type:'success'})})
 }
 
 export default useEventSubscriptions
