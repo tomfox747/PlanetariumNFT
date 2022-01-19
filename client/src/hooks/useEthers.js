9d7fe78710432ddff14cb59ec9f73b532968aa46
Thomas Fox <47598118+tomfox747@users.noreply.github.com>
2022-01-19 21:22:10 +0000

    many fixes

--- client/src/hooks/useEthers.js
+++ client/src/hooks/useEthers.js
@@ -1,6 +1,7 @@
 import {useState,useEffect} from 'react'
 import { ethers } from 'ethers'
 import { useMoralis } from 'react-moralis'
+import { toast } from 'react-toastify'
 
 const localnetId = 31337
 const testnetId = 43113
@@ -18,30 +19,42 @@ const useEthers = () => {
     const {enableWeb3, authenticate} = useMoralis()
 
     const connectWallet = async () => {
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-        setAddress(accounts[0]);
-        setProvider(new ethers.providers.Web3Provider(window.ethereum, "any"))
-        authenticate()
+        try{
+            const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+            setAddress(accounts[0]);
+            setProvider(new ethers.providers.Web3Provider(window.ethereum, "any"))
+            authenticate()
+        }catch(e){
+            //toast("Error connecting to wallet, please install metamask", {type:'warning', autoClose:'10000'})
+        }
     }
 
     useEffect(() => {
-        window.ethereum.on('accountsChanged', async (event, accounts) =>  {
-            setAddress(accounts)
-            window.location.reload()
-        })
-          
-        window.ethereum.on('chainChanged', function (networkId) {
-            let id = parseInt(networkId.toString())
-            if(id !== envChain){
-                setChain(false)
-            }else{
-                setChain(true)
-            }
-        })
-        enableWeb3()
-        authenticate()
-        connectWallet()
-        setFirstRender(false)
+        try{
+            window.ethereum.on('accountsChanged', async (event, accounts) =>  {
+                setAddress(accounts)
+                window.location.reload()
+            })
+              
+            window.ethereum.on('chainChanged', function (networkId) {
+                let id = parseInt(networkId.toString())
+                if(id !== envChain){
+                    setChain(false)
+                }else{
+                    setChain(true)
+                }
+            })
+        }catch(e){
+            toast("Error connecting to blockchain, please install metamask", {type:'warning', autoClose:'10000'})
+        }
+        try{
+            enableWeb3()
+            authenticate()
+            connectWallet()
+            setFirstRender(false)
+        }catch(e){
+            toast("Error connecting to blockchain, please install metamask", {type:'warning', autoClose:'10000'})
+        }
     },[])
 
     useEffect(() => {
