8e9642f5d0b685af2bffe4856fab26991050a456
Thomas Fox <47598118+tomfox747@users.noreply.github.com>
2022-01-19 00:33:06 +0000

    background randomise

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,10 +10,15 @@ import { ToastContainer } from 'react-toastify';
 
 import GalaxyNFT from './contracts/abis/GalaxyNFT'
 
-import Background from './assets/background.jpg'
-import useEventSubscriptions from "hooks/useEventSubscriptions";
+import Background1 from './assets/background.jpg'
+import Background2 from './assets/background2.jpg'
+import Background3 from './assets/background3.jpg'
+import Background4 from './assets/background4.jpg'
+import Background5 from './assets/background5.jpg'
 
 import 'react-toastify/dist/ReactToastify.css';
+
+const images = [Background1,Background2, Background3, Background4, Background5]
 
 const getWindowDimensions = () => {
   const{innerWidth: width, innerHeight:height} = window;
@@ -37,9 +42,15 @@ const App = ({ isServerInfo }) => {
   const {windowSize} = useWindowSize()
   const {connectWallet, address, chain} = useEthers()
   const [currentMarketplace, setCurrentMarketplace] = useState({GalaxyNFT})
+  const [img, setimg] = useState(null)
+
+  useEffect(() => {
+    let num = Math.floor(Math.random() * 5);
+    setimg(images[num])
+  },[])
 
   return (
-    <div style={{backgroundColor:'black', backgroundImage:`url(${Background})`, height:'100vh', overflow:'hidden', maxHeight:windowSize.height, width:'100%', padding:'0px', margin:'0px'}}>
+    <div style={{backgroundColor:'black', backgroundImage:`url(${img})`, backgroundSize:'cover', height:'100vh', overflow:'hidden', maxHeight:windowSize.height, width:'100%', padding:'0px', margin:'0px'}}>
       <WindowSizeStore.Provider value={{windowSize}}>
         <EthersStore.Provider value={{connectWallet, address, chain}}>
           <MarketplaceStore.Provider value={{currentMarketplace, setCurrentMarketplace}}>
